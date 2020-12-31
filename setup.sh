0a52889c99c1c0fefc9a0972aa5895e32033f53d
Justas Stankevicius <juuuuuustas@gmail.com>
2020-12-30 22:33:50 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 sudo usermod -aG docker $USER
-newgrp docker
-exit
-echo "modified docker perms"
 sudo apt-get install python3-pip
 pip3 install -r requirements.txt
 cd ~
@@ -9,6 +6,5 @@ mkdir go
 sudo mount /dev/sda4 go
 sudo chmod -R 777 go
 mkdir -p go/src/github.com/cockroachdb
-echo "created go build path"
 cd go/src/github.com/cockroachdb 
 git clone https://github.com/jstankevicius/cockroach

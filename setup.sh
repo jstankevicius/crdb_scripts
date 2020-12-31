6a414f23b921013adba6fd4dcb416ae77bc3ea60
justas <justas@workload-node.crdb-cluster.uic-dcs-pg0.utah.cloudlab.us>
2020-12-30 21:18:06 -0700

    add requirement install to setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@ sudo usermod -aG docker $USER
 newgrp docker
 exit
 echo "modified docker perms"
+sudo apt-get install python3-pip
+pip3 install -r requirements.txt
+cd ~
 mkdir go
 sudo mount /dev/sda4 go
 sudo chmod -R 777 go

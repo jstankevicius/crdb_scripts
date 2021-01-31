34714e9eae7e40659c1e0a600f74087c42786be5
jstankevicius <juuuuuuustas@gmail.com>
2021-01-31 02:17:26 -0700

    add git config to setup and re-add config.yaml

--- setup.sh
+++ setup.sh
@@ -6,5 +6,7 @@ mkdir go
 sudo mount /dev/sda4 go
 sudo chmod -R 777 go
 mkdir -p go/src/github.com/cockroachdb
-cd go/src/github.com/cockroachdb 
+cd go/src/github.com/cockroachdb
+git config --global user.name "jstankevicius"
+git config --globla user.email "juuuuuustas@gmail.com"
 git clone https://github.com/jstankevicius/cockroach
